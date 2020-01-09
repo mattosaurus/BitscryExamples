c16343a12031c978e1a1c32311792ee39dc10a57
mattosaurus <mattosaurus@outlook.com>
2020-01-09 17:10:58 +0000

    Add project files.

---
+++ ConsoleAppSettings/App.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSettings
+{
+    public class App
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly ILogger<App> _logger;
+
+        public App(IConfigurationRoot config, ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<App>();
+            _config = config;
+        }
+
+        public async Task Run()
+        {
+            List<string> emailAddresses = _config.GetSection("EmailAddresses").Get<List<string>>();
+            foreach (string emailAddress in emailAddresses)
+            {
+                _logger.LogInformation("Email address: {@EmailAddress}", emailAddress);
+            }
+        }
+    }
+}
---
+++ ConsoleAppSettings/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSettings
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+
+        static int Main(string[] args)
+        {
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console(Serilog.Events.LogEventLevel.Debug)
+                 .MinimumLevel.Debug()
+                 .Enrich.FromLogContext()
+                 .CreateLogger();
+
+            try
+            {
+                // Start!
+                MainAsync(args).Wait();
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            // Create service collection
+            Log.Information("Creating service collection");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Create service provider
+            Log.Information("Building service provider");
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            try
+            {
+                Log.Information("Starting service");
+                await serviceProvider.GetService<App>().Run();
+                Log.Information("Ending service");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running service");
+                throw ex;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Add logging
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddSerilog(dispose: true);
+            }));
+
+            serviceCollection.AddLogging();
+
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            // Add app
+            serviceCollection.AddTransient<App>();
+        }
+    }
+}
