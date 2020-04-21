3754ecfba5d0049d298079813c2b95cde6cd68c3
mattosaurus <bitscry@outlook.com>
2020-04-21 12:12:31 +0100

    Add infrastructure classes

---
+++ AADGroupAuthorization/Infrastructure/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADGroupAuthorization.Infrastructure
+{
+    public static class Constants
+    {
+        public const string ScopeUserRead = "User.Read";
+        public const string ScopeUserReadAll = "User.ReadBasic.All";
+        public const string BearerAuthorizationScheme = "Bearer";
+        public const string ScopeDirectoryReadAll = "Directory.Read.All";
+    }
+}
---
+++ AADGroupAuthorization/Infrastructure/GraphScopes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADGroupAuthorization.Infrastructure
+{
+    public static class GraphScopes
+    {
+        public const string UserRead = "User.Read";
+        public const string UserReadBasicAll = "User.ReadBasic.All";
+        public const string DirectoryReadAll = "Directory.Read.All";
+    }
+}
