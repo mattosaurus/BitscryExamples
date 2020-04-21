990d120f2c08d780bcd26e23b0c8f7fb158f5aed
mattosaurus <bitscry@outlook.com>
2020-04-21 15:31:38 +0100

    Add authorization and profile

--- AADGroupAuthorization/Controllers/HomeController.cs
+++ AADGroupAuthorization/Controllers/HomeController.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AADGroupAuthorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using AADGroupAuthorization.Services;
+using Microsoft.Identity.Web;
+using Microsoft.Graph;
 
 namespace AADGroupAuthorization.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IMSGraphService _graphService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITokenAcquisition tokenAcquisition, IMSGraphService graphService)
         {
             _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+            _graphService = graphService;
         }
 
         public IActionResult Index()
@@ -23,8 +32,34 @@ namespace AADGroupAuthorization.Controllers
             return View();
         }
 
+        [Authorize(Roles = "<YOURGROUPID>")]
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        [AuthorizeForScopes(Scopes = new[] { Infrastructure.Constants.ScopeUserRead })]
+        public async Task<IActionResult> Profile()
+        {
+            string token = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { Infrastructure.Constants.ScopeUserRead, Infrastructure.Constants.ScopeDirectoryReadAll });
+
+            User me = await _graphService.GetMeAsync(token);
+            ViewData["Me"] = me;
+
+            try
+            {
+                // Get user photo
+                ViewData["Photo"] = await _graphService.GetMyPhotoAsync(token);
+            }
+            catch (System.Exception)
+            {
+                ViewData["Photo"] = null;
+            }
+
+            IList<Group> groups = await _graphService.GetMyMemberOfGroupsAsync(token);
+
+            ViewData["Groups"] = groups;
+
             return View();
         }
 
