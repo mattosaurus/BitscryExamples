fdecf72a8c4e7885f6b649fe0f98b81523c99ab3
mattosaurus <bitscry@outlook.com>
2020-04-21 15:31:06 +0100

    Add AccountController for AccessDenied page

---
+++ AADGroupAuthorization/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AADGroupAuthorization.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
